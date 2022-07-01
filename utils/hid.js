53145f24907a5930a7d7d7fed667bf4ccf6d9708
GusFors <gf222cy@student.lnu.se>
2022-07-01 17:19:19 +0200

    try find tablet hidraw path with without node-hid

---
+++ utils/hid.js
@@ -0,0 +1,30 @@
+const fs = require('fs/promises')
+
+;(async () => {
+  let hidrawDirs = await fs.readdir('/sys/class/hidraw')
+
+  let foundTablets = []
+  for (let i = 0; i < hidrawDirs.length; i++) {
+    let currentDevice = await fs.readFile(`/sys/class/hidraw/${hidrawDirs[i]}/device/uevent`, { encoding: 'utf8' })
+    let r = currentDevice.split('\n')
+    let isTablet = false
+    let tablet = {}
+
+    for (let y = 0; y < r.length; y++) {
+      let rInfo = r[y]
+      let devName
+      if (rInfo.includes('0000056A')) {
+        isTablet = true
+        break
+      }
+    }
+
+    if (isTablet) {
+      tablet.info = currentDevice
+      tablet.hidpath = hidrawDirs[i]
+      foundTablets.push(tablet)
+    }
+  }
+
+  console.log(foundTablets)
+})()
