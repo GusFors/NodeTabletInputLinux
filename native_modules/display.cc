930f2048b3cb24938e7042b6d35abe49a2812e16
GusFors <gustav.forselius@gmail.com>
2024-02-01 23:42:27 +0100

    display functions wrappers so they also can be called directly from native addon

--- native_modules/display.cc
+++ native_modules/display.cc
@@ -5,10 +5,73 @@
 #include <X11/extensions/XTest.h>
 
 Display *display = NULL;
+// place nan.h first if rebuild fails
 
-// TODO fix an actual formatter for c++, place nan.h first after formatting or
-// rebuild might fail
-NAN_METHOD(getNumberOfMonitors) {
+int get_displays_total_width() {
+  if (display == NULL) {
+    display = XOpenDisplay(NULL);
+  }
+
+  int displayWidth = XDisplayWidth(display, 0);
+
+  return displayWidth;
+}
+
+int get_displays_total_height() {
+  if (display == NULL) {
+    display = XOpenDisplay(NULL);
+  }
+
+  int displayHeight = XDisplayHeight(display, 0);
+
+  return displayHeight;
+}
+
+int get_primary_monitor_xoffset() {
+  if (display == NULL) {
+    display = XOpenDisplay(NULL);
+  }
+
+  XRRScreenResources *monRes = XRRGetScreenResources(display, XDefaultRootWindow(display));
+  XRRCrtcInfo *monInfo = XRRGetCrtcInfo(display, monRes, monRes->crtcs[0]);
+
+  return monInfo->x;
+}
+
+int get_primary_monitor_yoffset() {
+  if (display == NULL) {
+    display = XOpenDisplay(NULL);
+  }
+
+  XRRScreenResources *monRes = XRRGetScreenResources(display, XDefaultRootWindow(display));
+  XRRCrtcInfo *monInfo = XRRGetCrtcInfo(display, monRes, monRes->crtcs[0]);
+
+  return monInfo->y;
+}
+
+int get_primary_monitor_width() {
+  if (display == NULL) {
+    display = XOpenDisplay(NULL);
+  }
+
+  XRRScreenResources *monRes = XRRGetScreenResources(display, XDefaultRootWindow(display));
+  XRRCrtcInfo *monInfo = XRRGetCrtcInfo(display, monRes, monRes->crtcs[0]);
+
+  return monInfo->width;
+}
+
+int get_primary_monitor_height() {
+  if (display == NULL) {
+    display = XOpenDisplay(NULL);
+  }
+
+  XRRScreenResources *monRes = XRRGetScreenResources(display, XDefaultRootWindow(display));
+  XRRCrtcInfo *monInfo = XRRGetCrtcInfo(display, monRes, monRes->crtcs[0]);
+
+  return monInfo->height;
+}
+
+int get_number_of_monitors() {
   if (display == NULL) {
     display = XOpenDisplay(NULL);
   }
@@ -16,91 +79,56 @@ NAN_METHOD(getNumberOfMonitors) {
   int numMonitors = 0;
   XRRGetMonitors(display, XDefaultRootWindow(display), 1, &numMonitors);
 
-  info.GetReturnValue().Set(Nan::New(numMonitors));
+  return numMonitors;
 }
 
 NAN_METHOD(getDisplaysTotalWidth) {
-  if (display == NULL) {
-    display = XOpenDisplay(NULL);
-  }
-
-  int x = 0;
-  int displayWidth = XDisplayWidth(display, 0);
-
+  int displayWidth = get_displays_total_width();
   // auto Scr = DefaultScreen(display);
   // int xWidth = XWidthOfScreen(ScreenOfDisplay(display, Scr));
-
+  // XCloseDisplay(display);
   info.GetReturnValue().Set(Nan::New(displayWidth));
 }
 
 NAN_METHOD(getDisplaysTotalHeight) {
-  if (display == NULL) {
-    display = XOpenDisplay(NULL);
-  }
-
-  int x = 0;
-  int displayHeight = XDisplayHeight(display, 0);
-
+  int displayHeight = get_displays_total_height();
   info.GetReturnValue().Set(Nan::New(displayHeight));
 }
 
 NAN_METHOD(getPrimaryMonitorXoffset) {
-  if (display == NULL) {
-    display = XOpenDisplay(NULL);
-  }
-
-  XRRScreenResources *monRes = XRRGetScreenResources(display, XDefaultRootWindow(display));
-
-  XRRCrtcInfo *monInfo = XRRGetCrtcInfo(display, monRes, monRes->crtcs[0]);
-
-  info.GetReturnValue().Set(Nan::New(monInfo->x));
+  int xoffset = get_primary_monitor_xoffset();
+  info.GetReturnValue().Set(Nan::New(xoffset));
 }
 
 NAN_METHOD(getPrimaryMonitorYoffset) {
-  if (display == NULL) {
-    display = XOpenDisplay(NULL);
-  }
-
-  XRRScreenResources *monRes = XRRGetScreenResources(display, XDefaultRootWindow(display));
-
-  XRRCrtcInfo *monInfo = XRRGetCrtcInfo(display, monRes, monRes->crtcs[0]);
-
-  info.GetReturnValue().Set(Nan::New(monInfo->y));
+  int yoffset = get_primary_monitor_yoffset();
+  info.GetReturnValue().Set(Nan::New(yoffset));
 }
 
 NAN_METHOD(getPrimaryMonitorWidth) {
-  if (display == NULL) {
-    display = XOpenDisplay(NULL);
-  }
-
-  XRRScreenResources *monRes = XRRGetScreenResources(display, XDefaultRootWindow(display));
-
-  XRRCrtcInfo *monInfo = XRRGetCrtcInfo(display, monRes, monRes->crtcs[0]);
-
-  info.GetReturnValue().Set(Nan::New(monInfo->width));
+  int width = get_primary_monitor_width();
+  info.GetReturnValue().Set(Nan::New(width));
 }
 
 NAN_METHOD(getPrimaryMonitorHeight) {
-  if (display == NULL) {
-    display = XOpenDisplay(NULL);
-  }
+  int height = get_primary_monitor_height();
+  info.GetReturnValue().Set(Nan::New(height));
+}
 
-  XRRScreenResources *monRes = XRRGetScreenResources(display, XDefaultRootWindow(display));
-
-  XRRCrtcInfo *monInfo = XRRGetCrtcInfo(display, monRes, monRes->crtcs[0]);
-
-  info.GetReturnValue().Set(Nan::New(monInfo->height));
+NAN_METHOD(getNumberOfMonitors) {
+  int numMonitors = get_number_of_monitors();
+  info.GetReturnValue().Set(Nan::New(numMonitors));
 }
 
 // expose as a node module
 NAN_MODULE_INIT(init) {
   Nan::SetMethod(target, "getDisplaysTotalWidth", getDisplaysTotalWidth);
   Nan::SetMethod(target, "getDisplaysTotalHeight", getDisplaysTotalHeight);
-  Nan::SetMethod(target, "getNumberOfMonitors", getNumberOfMonitors);
   Nan::SetMethod(target, "getPrimaryMonitorXoffset", getPrimaryMonitorXoffset);
   Nan::SetMethod(target, "getPrimaryMonitorYoffset", getPrimaryMonitorYoffset);
   Nan::SetMethod(target, "getPrimaryMonitorWidth", getPrimaryMonitorWidth);
   Nan::SetMethod(target, "getPrimaryMonitorHeight", getPrimaryMonitorHeight);
+  Nan::SetMethod(target, "getNumberOfMonitors", getNumberOfMonitors);
 }
 
 NODE_MODULE(display, init);
