06facac2055e0ddbd46dd80e8f0d853139c12fbc
GusFors <gustav.forselius@gmail.com>
2024-03-20 17:47:14 +0100

    add temporary function for converting from mm config

--- native_modules/conf/config_handler.c
+++ native_modules/conf/config_handler.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include "../display.h"
 #include "../tablet.h"
+#include "config_handler.h"
 
 struct tablet_config get_tablet_config(int vendor, int product) {
   FILE *conf_file;
@@ -73,6 +74,84 @@ struct tablet_config get_tablet_config(int vendor, int product) {
   return tablet_conf;
 }
 
+struct tablet_config get_tablet_mmconfig(int vendor, int product) {
+  FILE *conf_file;
+  conf_file = fopen("./conf/mmtablets.conf", "r");
+
+  if (conf_file == NULL) {
+    printf("Err when opening file");
+    exit(1);
+  }
+
+  struct tablet_config tablet_conf;
+  char line[128] = "";
+  int vend_match = 0;
+  int prod_match = 0;
+  double w = 0;
+  double h = 0;
+  double x = 0;
+  double y = 0;
+
+  while ((fgets(line, 128, conf_file)) != NULL) {
+    char *key = strtok(line, "=");
+    char *strvalue = strtok(NULL, "=");
+
+    if (vend_match == 1 && prod_match == 1 && strvalue == NULL) {
+      printf("matched vendor: %d, matched product: %d\n", vend_match, prod_match);
+      break;
+    }
+
+    if (strvalue != NULL) {
+      double value = strtod(strvalue, NULL);
+
+      // check trunc
+      if (strcmp("width", key) == 0) {
+        w = value;
+      } else if (strcmp("height", key) == 0) {
+        h = value;
+      } else if (strcmp("xoffset", key) == 0) {
+        x = value;
+      } else if (strcmp("yoffset", key) == 0) {
+        y = value;
+      } else if (strcmp("xindex", key) == 0) {
+        tablet_conf.xindex = value;
+      } else if (strcmp("yindex", key) == 0) {
+        tablet_conf.yindex = value;
+      } else if (strcmp("vendor", key) == 0 && value == vendor) {
+        vend_match = 1;
+      } else if (strcmp("product", key) == 0 && value == product) {
+        prod_match = 1;
+      }
+
+      printf("%s=%f\n", key, value);
+    }
+  }
+
+  fclose(conf_file);
+
+  if (vend_match == 0 || prod_match == 0) {
+    printf("\nfailed to find a matching tablet config\n");
+    exit(0);
+  }
+
+  int left = (x - w / 2) * 100;
+  int right = (x + w / 2) * 100;
+  int top = (y - h / 2) * 100;
+  int bottom = (y + h / 2) * 100;
+
+  tablet_conf.left = left;
+  tablet_conf.right = right;
+  tablet_conf.top = top;
+  tablet_conf.bottom = bottom;
+  tablet_conf.xscale = 0.32;
+  tablet_conf.yscale = 0.32;
+
+  printf("\nconverted config\n");
+  print_tablet_config(tablet_conf);
+
+  return tablet_conf;
+}
+
 struct display_config get_display_config() {
   struct display_config display_conf;
   display_conf.offset_x = 2560;
@@ -83,4 +162,9 @@ struct display_config get_display_config() {
   display_conf.total_width = 7680;
 
   return display_conf;
+}
+
+void print_tablet_config(struct tablet_config cfg) {
+  printf("left: %d\nright: %d\n", cfg.left, cfg.right);
+  printf("top: %d\nbottom: %d\n", cfg.top, cfg.bottom);
 }
--- native_modules/conf/config_handler.h
+++ native_modules/conf/config_handler.h
@@ -9,6 +9,8 @@ extern "C" {
 
 // struct tablet_config get_tablet_config(const char *tablet_name);
 struct tablet_config get_tablet_config(int vendor, int product);
+struct tablet_config get_tablet_mmconfig(int vendor, int product);
+void print_tablet_config(struct tablet_config cfg);
 struct display_config get_display_config();
 
 #ifdef __cplusplus
